a16a7da13432113f87d4bef11aa0d83d577f67e5
Alexsonfilgueiras <alexsonfilgueiras@gmail.com>
2026-02-16 19:38:11 +0000

    Versao01Carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,13 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+      EstadoCarta1, EstadoCarta2 char[];
+      CodigoCarta1,CodigoCarta2 char[3]; //A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)//
+      NomeCidadecarta1, NomeCidadecarta2 char[20];  //O nome da cidade. Tipo: char[] (string)// 
+      PopulacaoCarta1, PopulacaoCarta2 int; //O número de habitantes da cidade. Tipo: int// 
+      AreaCarta1, AreaCarta2 float; //A área da cidade em quilômetros quadrados. Tipo: float// 
+      pibCarta1, pibCarta2 float; //O Produto Interno Bruto da cidade. Tipo: float//
+      NumPontoTuristicoCarta1, NumPontoTuristicoCarta2 int; //A quantidade de pontos turísticos na cidade. Tipo: int//
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
